3e806193c8e59221ff376a2b8f6cfc67c42dce46
mohit <mohitul449@gmail.com>
2025-06-08 18:21:12 +0600

    added some roles for table data

--- cmd/migrate/migrations/000002_roles_table.up.sql
+++ cmd/migrate/migrations/000002_roles_table.up.sql
@@ -1,10 +1,15 @@
 CREATE TABLE roles (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL UNIQUE,     -- 'admin', 'cashier', etc.
-  code VARCHAR(50) DEFAULT NULL,        -- optional slug-like alias
-  is_active BOOLEAN DEFAULT TRUE,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE, -- 'admin', 'cashier', etc.
+    code VARCHAR(50) DEFAULT NULL, -- optional slug-like alias
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_roles_is_active ON roles(is_active);
+CREATE INDEX idx_roles_is_active ON roles (is_active);
+
+-- INSERT INTO roles (name, code) VALUES
+--   ('Admin', 'admin'),
+--   ('Employee', 'employee'),
+--   ('Cashier', 'cashier');
