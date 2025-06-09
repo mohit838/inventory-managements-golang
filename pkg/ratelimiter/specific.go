7b0b50d9da582c3a37b61fd83cfb0a714497a61d
mohit <mohitul449@gmail.com>
2025-06-09 14:05:01 +0600

    refactor ratelimiter middleware

--- pkg/ratelimiter/specific.go
+++ pkg/ratelimiter/specific.go
@@ -2,20 +2,20 @@ package ratelimit
 
 import (
 	"github.com/gin-gonic/gin"
-	limiter "github.com/ulule/limiter/v3"
+	limiterPkg "github.com/ulule/limiter/v3"
 	mgin "github.com/ulule/limiter/v3/drivers/middleware/gin"
 	memory "github.com/ulule/limiter/v3/drivers/store/memory"
 )
 
 func RefreshRateLimitMiddleware() gin.HandlerFunc {
-	rateStr := "3-M" // 3 requests per minute per IP
-	rate, err := limiter.NewRateFromFormatted(rateStr)
+	rate, err := limiterPkg.NewRateFromFormatted("3-M")
 	if err != nil {
 		panic(err)
 	}
 
 	store := memory.NewStore()
-	middleware := mgin.NewMiddleware(limiter.New(store, rate))
+	limiterInstance := limiterPkg.New(store, rate)
+	middleware := mgin.NewMiddleware(limiterInstance)
 
 	return middleware
 }
